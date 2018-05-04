e1bd61f323f5e6006f0d8ec94552b60c0b8e2519
Callum Barratt <callum@barratt.me>
2018-05-04 12:16:32 +0100

    First pass at actually getting the data right

--- Gemfile
+++ Gemfile
@@ -14,6 +14,10 @@ gem 'coffee-rails', '~> 4.2'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 
+gem 'nokogiri'
+
+gem 'pry'
+
 # Reduces boot times through caching; required in config/boot.rb
 # gem 'bootsnap', '>= 1.1.0', require: false
 
--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,5 +1,25 @@
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
 class TestController < ApplicationController
   def index
-    render(json: { hello: 'world' })
+    blah = collection_day
+
+    render json: { dates: blah }
+  end
+
+  def collection_day
+    doc = Nokogiri::HTML(open('http://www.wakefield.gov.uk/site/Where-I-Live-Results?uprn=63004110'))
+
+    collection_dates = doc.css('.mb10 .ind-waste-wrapper', '.col-sm-4').map do |date|
+      date.children[1].content if date.children[1]
+    end.compact
+
+    {
+      household: collection_dates.slice!(0..1).sort_by { |string| Date.parse(string) },
+      mixed: collection_dates.slice!(0..1).sort_by { |string| Date.parse(string) },
+      garden: collection_dates.slice!(0..1).sort_by { |string| Date.parse(string) }
+    }
   end
 end
