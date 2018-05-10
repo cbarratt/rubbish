e87446cb7644a216cbec231e8a58a4d42133a21c
Callum Barratt <callum@barratt.me>
2018-05-10 09:46:13 +0100

    Refactor pt 1

--- lib/collection_dates.rb
+++ lib/collection_dates.rb
@@ -1,26 +1,28 @@
 module CollectionDates
-  def self.fetch
-    doc = Nokogiri::HTML(get_page_html)
+  class << self
+    def fetch
+      doc = Nokogiri::HTML(get_page_html)
 
-    collection_dates = doc.css('.mb10 .ind-waste-wrapper', '.col-sm-4').map do |date|
-      date.children[1].content if date.children[1]
-    end.compact
+      collection_dates = doc.css('.mb10 .ind-waste-wrapper', '.col-sm-4').map do |date|
+        date.children[1].content if date.children[1]
+      end.compact
 
-    {
-      household: collection_dates.slice!(0..1).map { |string| parse_date(string) }.sort,
-      mixed: collection_dates.slice!(0..1).map { |string| parse_date(string) }.sort,
-      garden: collection_dates.slice!(0..1).map { |string| parse_date(string) }.sort
-    }
-  end
+      {
+        household: collection_dates.slice!(0..1).map { |string| parse_date(string) }.sort,
+        mixed: collection_dates.slice!(0..1).map { |string| parse_date(string) }.sort,
+        garden: collection_dates.slice!(0..1).map { |string| parse_date(string) }.sort
+      }
+    end
 
-  def self.get_page_html
-    HTTP.get('http://www.wakefield.gov.uk/site/Where-I-Live-Results?uprn=63004110').to_s
-  end
+    def get_page_html
+      HTTP.get('http://www.wakefield.gov.uk/site/Where-I-Live-Results?uprn=63004110').to_s
+    end
 
-  def self.parse_date(string_date)
-    return Date.today.to_s if string_date == 'Today'
-    return Date.yesterday.to_s if string_date == 'Yesterday'
+    def parse_date(string_date)
+      return Date.today.to_s if string_date == 'Today'
+      return Date.yesterday.to_s if string_date == 'Yesterday'
 
-    Date.parse(string_date).to_s
+      Date.parse(string_date).to_s
+    end
   end
 end
