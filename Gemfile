99f6c112bbe0bd732c9ddb41d73782f2b4eb10ab
Callum Barratt <callum@barratt.me>
2018-05-11 10:46:06 +0100

    Bypass cache

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '2.5.1'
+ruby IO.read('.ruby-version').strip
 
 gem 'rails', '~> 5.2.0'
 
--- app/controllers/api/collection_controller.rb
+++ app/controllers/api/collection_controller.rb
@@ -2,8 +2,12 @@ require_dependency './lib/collection_dates'
 
 class Api::CollectionController < Api::BaseController
   def index
-    collections = Rails.cache.fetch("dates", expires_in: 60.minutes) do
-      ::CollectionDates.fetch
+    if params[:cached] == 'true'
+      collections = Rails.cache.fetch("dates", expires_in: 60.minutes) do
+        ::CollectionDates.fetch
+      end
+    else
+      collections = ::CollectionDates.fetch
     end
 
     render json: { dates: collections }
